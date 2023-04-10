1acf61835125084ee4bbb6bff5fff284d41cae78
Nessa92130377 <www.confi-esdeuma-estudante>
2023-04-10 13:56:22 -0300

    My Django Girls app, first commit

---
+++ blog/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blog'
+#token github: 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
